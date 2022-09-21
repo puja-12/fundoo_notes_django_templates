053691edc270d37d5f1848aadd06aaa922581eb6
puja <pujarana1796@gmail.com>
2022-09-21 15:48:27 +0530

    [puja-added] added middleware

--- user/middleware.py
+++ user/middleware.py
@@ -1,4 +1,5 @@
 from django.db import connection
+from django.db.models import Count
 
 from user.models import MiddlewareDetails
 
@@ -8,30 +9,34 @@ class NoteMiddleware:
         self.get_response = get_response
         # One-time configuration and initialization.
 
-    def __call__(self, request,*args,**kwargs):
+    def __call__(self, request, *args, **kwargs):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
 
-        # response = self.get_response(request)
-
-        # user=MiddlewareDetails.objects.values("url")
-        # print(user.url)
-        if request.method == 'POST':
-            method = request.method
-            url = request.path
-            count = url.count("url")
-            # print(count , "jfdvj")
-            # print(method)
-            # print(url)
-            if count <= 0:
-                # count = url.count("url")
-                MiddlewareDetails.objects.create(request_method=method, url=url,count=count)
-                # print(count)
-            else:
-
-
-                count = count + 1
-                # print(count)
-
         response = self.get_response(request)
+
+        # if request.method == 'POST':
+        method = request.method
+        url = request.path
+        count = MiddlewareDetails.objects.values('url').annotate(Count('url'))
+        # count1 = url.count("url")
+        print(count, "count")
+
+        print("values", count[0]['url__count'])
+        c = count[0]['url__count']
+        print(c)
+
+        if count == 0:
+
+            MiddlewareDetails.objects.create(request_method=method, url=url, count=count)
+            print("inside")
+
+
+        else:
+            count =+  1
+            print(count)
+            a = MiddlewareDetails.objects.update( count=c)
+            print("update", a)
+
+        # response = self.get_response(request)
         return response
